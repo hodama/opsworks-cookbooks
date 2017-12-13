7b5ec65a006a9a2af84ea4bac1b7664f62fffbc7
Alex Vidchinkin <ulmen.ua@gmail.com>
2017-12-13 16:22:08 +0200

    algolia rake task during deploy

--- deploy/recipes/rails-restart.rb
+++ deploy/recipes/rails-restart.rb
@@ -10,17 +10,21 @@ node[:deploy].each do |application, deploy|
     Chef::Log.debug("Skipping deploy::rails-restart application #{application} as it is not a Rails app")
     next
   end
-  
+
+  execute "set ALGOLIA indices settings" do
+    cwd deploy[:current_path]
+    command "bundle exec rake algolia:set_settings"
+    environment 'RAILS_ENV' => 'production'
+  end
+
   execute "restart Server" do
     cwd deploy[:current_path]
     command "sleep #{deploy[:sleep_before_restart]} && #{node[:opsworks][:rails_stack][:restart_command]}"
     action :run
-    
-    only_if do 
+
+    only_if do
       File.exists?(deploy[:current_path])
     end
   end
-    
+
 end
-
-
